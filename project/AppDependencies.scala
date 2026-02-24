653bf622521e80af670c70bfee34a1c7c7396694
damien.butterworth <damienbutterworth@hotmail.co.uk>
2026-02-24 12:35:17 +0000

    Upgrade libraries: sbt-plugin → 3.0.10, bootstrap-backend-play-30 → 10.6.0
    Bumps the following dependencies:
      - org.playframework:sbt-plugin → 3.0.10
      - uk.gov.hmrc:bootstrap-backend-play-30 → 10.6.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   private val catsVersion      = "2.9.0"
   private val catsRetryVersion = "3.1.0"
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-backend-play-30"    % bootstrapVersion,
